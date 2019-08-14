6730608faffdc246a5afd000203d6ea4bc7fe396
Sravan Kumar Pala <sravankumarpala926@gmail.com>
2019-08-14 07:10:51 +0530

    deleting confidential file

--- Azure_CRMS/az-crms/src/main/java/com/cat/azcrms/service/CRMSPolicyServiceI.java
+++
@@ -1,9 +0,0 @@
-package com.cat.azcrms.service;
-
-import com.vera.sdk.Securer.PolicySet;
-
-public interface CRMSPolicyServiceI {
-	
-	PolicySet[] getAllPolicies(String jsonAsRequest);
-
-}
--- SpringBootLearning/src/main/java/com/springboot/SpringBootLearning/AzCRMSIntegration.java
+++
@@ -1,7 +0,0 @@
-package com.springboot.SpringBootLearning;
-
-public class AzCRMSIntegration {
-	
-	
-
-}
